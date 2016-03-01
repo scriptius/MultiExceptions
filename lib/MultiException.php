df58da009ef54828c0d4253da56a3f1f3be3a949
Viktor <levik04122008@yandex.ru>
2016-03-01 13:13:24 +0300

    1

---
+++ lib/MultiException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class MultiException extends \App\Exceptions\Core implements \ArrayAccess, \Iterator {
+
+    use TCollection;
+
+    public function getMessagesFromMultiexceptionToString()
+    {
+        $messagesMultiexceptions = '';
+        foreach($this as $k => $v){
+           $messagesMultiexceptions .=  $v->getMessage().PHP_EOL;
+        }
+        return $messagesMultiexceptions;
+    }
+
+    public function getDataForEditingFromPost(){
+return $this->getTrace()[0]['args'][0];
+
+    }
+}
